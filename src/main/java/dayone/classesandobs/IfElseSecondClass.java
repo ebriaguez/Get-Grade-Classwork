debf3e142c62ea77e950f86e78c041cfdfacfa81
Emily Briaguez-Agurto <emilyb.agurto@gmail.com>
2022-09-06 15:13:48 -0400

    finally got it!

--- src/main/java/dayone/classesandobs/IfElseSecondClass.java
+++ src/main/java/dayone/classesandobs/IfElseSecondClass.java
@@ -2,21 +2,23 @@ package dayone.classesandobs;
 
 public class IfElseSecondClass {
     public static void main(String[] args) {
-        System.out.println((int) (Math.random() * 100));
+//        System.out.println((int) (Math.random() * 100));
 
         int studentsGrade = (int) (Math.random() * 100);
 
         if (studentsGrade >= 90){
             System.out.println("You got an A!");
-        } else if (studentsGrade >= 80 && studentsGrade <= 90){
+        } else if (studentsGrade >= 80 && studentsGrade < 90){
             System.out.println("You got a B");
-        } else if(studentsGrade >= 70 && studentsGrade <= 80){
+        } else if(studentsGrade >= 70 && studentsGrade < 80){
             System.out.println("You got a C");
-        } else if (studentsGrade >= 60 && studentsGrade <= 70){
+        } else if (studentsGrade >= 60 && studentsGrade < 70){
             System.out.println("You got a D");
         } else {
             System.out.println("You got an F");
         }
+
+        System.out.println(studentsGrade);
 
     }
 
